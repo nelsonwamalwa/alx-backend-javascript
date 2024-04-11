230fee53deb4ea238e1ff64c3e7aa2617a9b83e5
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-11 17:25:20 +0300

    Final Fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@ def pascal_triangle(n):
         return []
 
     triangle = [[1]]
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
@@ -11,15 +12,4 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
-
-# Test the function
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
 
